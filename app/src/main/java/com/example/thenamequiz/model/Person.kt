015d107e61bfe060684f58d199647ba7f90a1c20
Niklas Johansson <576966@stud.hvl.no>
2021-02-08 13:32:33 +0100

    Entity

--- app/src/main/java/com/example/thenamequiz/model/Person.kt
+++ app/src/main/java/com/example/thenamequiz/model/Person.kt
@@ -1,7 +1,14 @@
 package com.example.thenamequiz.model
 
 import android.graphics.Bitmap
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
+class Person(var name: String, var image: Bitmap){
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name="id", index = true)
+    val id: Long = 0L
 
-class Person(var name: String, var image: Bitmap)
+}
 
