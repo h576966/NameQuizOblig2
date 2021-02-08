369f76dc13b8601f8f1c24bf1b941fc514cd64d8
Niklas Johansson <576966@stud.hvl.no>
2021-02-08 11:23:32 +0100

    Added Dao Interface

---
+++ app/src/main/java/com/example/thenamequiz/dao/PersonDao.kt
@@ -0,0 +1,27 @@
+package com.example.thenamequiz.dao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface PersonDao {
+    @Query("SELECT * FROM persons ORDER BY name")
+    fun getAll(): LiveData<List<Person>>
+
+    @Query("SELECT * FROM persons WHERE id = :personId")
+    fun getPerson(personId: String): LiveData<Person>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(person: Person)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(persons: List<Person>)
+
+    @Query("DELETE FROM persons WHERE id = :personId")
+    suspend fun delete(personId: String)
+}
+
+}
