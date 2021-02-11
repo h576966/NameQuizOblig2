06bed9c904a840ddc3a861712aab538f827541fc
Niklas Johansson <576966@stud.hvl.no>
2021-02-11 11:15:22 +0100

    one final commit on the same topic

--- app/src/main/java/com/example/thenamequiz/model/PersonRoomDatabase.kt
+++ app/src/main/java/com/example/thenamequiz/model/PersonRoomDatabase.kt
@@ -11,59 +11,28 @@ import com.example.thenamequiz.dao.PersonDao
 abstract class PersonRoomDatabase: RoomDatabase() {
     abstract val personDao: PersonDao
 
-    companion object{
+    companion object {
 
         @Volatile
         private var INSTANCE: PersonRoomDatabase? = null
 
-        fun getInstance(context: Context): PersonRoomDatabase{
-            synchronized(this){
+        fun getInstance(context: Context): PersonRoomDatabase {
+            synchronized(this) {
                 var instance = INSTANCE
 
 
-                if (instance ==null){
+                if (instance == null) {
                     instance = Room.databaseBuilder(
-                                    context.applicationContext,
-                                    PersonRoomDatabase::class.java, "person_database")
+                            context.applicationContext,
+                            PersonRoomDatabase::class.java, "person_database")
 
                             .fallbackToDestructiveMigration()
                             .build()
-                    INSTANCE= instance
+                    INSTANCE = instance
                 }
                 return instance
             }
 
         }
     }
-
-   /* companion object {
-        @Volatile
-        private var instance: PersonRoomDatabase? = null
-        private val LOCK = Any()
-
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
-            instance ?: buildDatabase(context).also { instance = it }
-        }
-
-        private fun buildDatabase(context: Context) Room.databaseBuilder(context,
-        PersonRoomDatabase::
-        class.java, "person-list.db")
-        .build()
-
-    }*/
 }
-
-/*private static PersonRoomDatabase INSTANCE;
-
- static PersonRoomDatabse getDatabase(final Context context){
-     if(INSTANCE == null){
-         synchronized(PersonRoomDatabase.class){
-             if(INSTANCE == null){
-                 INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                 PersonRoomDatabase.class, "person_database").build();
-             }
-         }
-     }
-     return INSTANCE;
-
-     }*/
